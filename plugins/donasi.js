3cc120e82530443736fdc930f3bd61c9aa309aff
MuhammadVikyArdiansyah <vikyardiansyah334@gmail.com>
2022-03-25 19:41:01 +0700

    v2

---
+++ plugins/donasi.js
@@ -0,0 +1,14 @@
+//Jangan Di Hapus Tapi kalo Di tambah Boleh
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+┌〔 Donasi • Pulsa Indosat Ooredoo〕
+├ Pulsa: 085735980188
+├ Pulsa: 085815212637
+└────
+`.trim(), watermark, 'Menu', '.menu', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
