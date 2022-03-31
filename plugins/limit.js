1ab57765e541333af1d24ad4b9793d1304c0b329
PATRICK297R <96850073+PATRICK297R@users.noreply.github.com>
2022-03-31 13:27:14 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -1,9 +1,31 @@
+const uploadImage = require('../lib/uploadImage')
+let fetch = require('node-fetch')
+
 let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`*${user.limit}* Limit\n*${user.exp}* XP\nLevel *${user.level}*\nRole *${user.role}*`)
+    let caption = `
+Limit: *${user.limit}*
+EXP: *${user.exp}*
+Level: *${user.level}*
+Role: *${user.role}*
+`.trim()
+    let pp = await(await fetch('https://telegra.ph/file/d4b916a1e698d2b951a0e.png')).buffer()
+    try {
+      pp = await ( await fetch(await conn.getProfilePicture(who))).buffer()
+    } catch (e) {
+    } finally {
+      await conn.reply(m.chat, caption, m, { contextInfo: {
+        externalAdReply: {
+          mediaUrl: 'https://youtu.be/-tKVN2mAKRI',
+          title: user.name,
+          body: 'Ky-Bot',
+          thumbnail: pp
+        }
+    }})
+  }
 }
 handler.help = ['my [@user]']
 handler.tags = ['xp']
